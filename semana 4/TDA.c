5ceafc73188b737239b89a6275199993b5b7dedf
Ghox19 <gonzalo.moncada19@gmail.com>
2023-04-06 13:55:03 -0400

    Crea borrar en tda

--- semana 4/TDA.c
+++ semana 4/TDA.c
@@ -7,11 +7,12 @@
     -> operaciones*/
 
 int main(){
-    int n;
+    int n,num;
     printf("cuantos puntos va a ingresar: ");
     scanf("%d",&n);
     punto *punto;
     punto = memoria(n);
     insertar_punto(punto,n);
     mostrar(punto,n);
+    n = borrar(punto,n);
 }
--- semana 4/struct.h
+++ semana 4/struct.h
@@ -21,6 +21,34 @@ void insertar_punto(punto *p,int n){
 
 void mostrar(punto *p,int n){
     for (int i = 0; i < n; i++){
-        printf("%d,%d\n",p[i].x,p[i].y);
+        printf("%d,%d ",p[i].x,p[i].y);
     }
+}
+
+int borrar(punto *p, int n){
+    int pos, temp1, temp2;
+    printf("\n\n");
+    for (int i = 0; i < n; i++){
+        printf("%d,%d ",p[i].x,p[i].y);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++){
+        printf("|%d| ",i+1);
+    }
+    printf("\nQue posicion quieres eliminar: ");
+    scanf("%d",&pos);
+    pos -= 1;
+    for (int j = pos; j < n; j++){
+        temp1 = p[j].x;
+        temp2 = p[j].y;
+        p[j].x = p[j + 1].x;
+        p[j].y = p[j + 1].y;
+        p[j + 1].x = temp1;
+        p[j + 1].y = temp2;
+    }
+    n -= 1;
+    for (int i = 0; i < n; i++){
+        printf("%d,%d ",p[i].x,p[i].y);
+    }
+    return n;
 }
